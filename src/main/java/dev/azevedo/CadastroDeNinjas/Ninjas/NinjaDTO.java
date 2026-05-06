5b94d6e5c39255079f88f199f87e6f6225f92582
Thiago Azevedo <thico.az@gmail.comm>
2026-05-06 14:13:14 -0300

    chore: Inclui a versao do DTO pela enitade que mapeia manualmente no NinjaMapper os caminha entidade x DTO

--- src/main/java/dev/azevedo/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/azevedo/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,27 @@
 package dev.azevedo.CadastroDeNinjas.Ninjas;
 
+import dev.azevedo.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+    private String rank;
+
+    private MissoesModel missoes;
+
+
 }
